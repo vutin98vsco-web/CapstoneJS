2843c6b056c31912f04d8728858a0384e797f2c1
vutin98vsco-web <vutin98.vsco@gmail.com>
2026-09-19 22:30:00 +0700

    format

---
+++ Capstone JS/src/js/utils/format.js
@@ -0,0 +1,12 @@
+export const formatCurrency = (value) => new Intl.NumberFormat("vi-VN", {
+  style: "currency",
+  currency: "VND",
+  maximumFractionDigits: 0,
+}).format(Number(value));
+
+export const escapeHtml = (value = "") => String(value)
+  .replaceAll("&", "&amp;")
+  .replaceAll("<", "&lt;")
+  .replaceAll(">", "&gt;")
+  .replaceAll('"', "&quot;")
+  .replaceAll("'", "&#039;");
